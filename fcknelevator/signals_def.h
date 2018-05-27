7a2b97407fb7de3531e1e4f351b8e0e339c60120
Nick <stroooooom@gmail.com>
2018-05-27 19:11:11 +0300

    HOTFIX ALARM

--- fcknelevator/signals_def.h
+++ fcknelevator/signals_def.h
@@ -3,13 +3,13 @@
 
 enum
 {
-    B1_CLICKED, // button_1 clicked
+    B1_CLICKED = 65, // button_1 clicked
     B2_CLICKED,
     B3_CLICKED,
     B4_CLICKED,
     B5_CLICKED,
 
-    LB1_CLICKED, // lift button_1 clicked
+    LB1_CLICKED,    // lift button_1 clicked
     LB2_CLICKED,
     LB3_CLICKED,
     LB4_CLICKED,
