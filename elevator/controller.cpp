8575dda1939dfbdc41a5e0221ea8c32783005d59
wh75er <bokseng12@gmail.com>
2018-05-29 14:27:08 +0300

    constructers added + model added in mainwindow

--- elevator/controller.cpp
+++ elevator/controller.cpp
@@ -1,6 +1,6 @@
 #include "controller.h"
 
-Controller::Controller()
+Controller::Controller(Lift *lift)
 {
-    QObject::connect(this, SIGNAL(newFloor(int)), cabin, );
+    QObject::connect(this, SIGNAL(newFloor(int)), lift->cabin, SLOT(getNewFloorSlot(int)));
 }
--- elevator/mainwindow.cpp
+++ elevator/mainwindow.cpp
@@ -11,6 +11,7 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    class Lift lift;
 }
 
 MainWindow::~MainWindow()
