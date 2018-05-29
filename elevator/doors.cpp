c81a6bc6b8b5fe9614a5dad6c8a205a95d052150
Nick <stroooooom@gmail.com>
2018-05-29 15:49:23 +0300

    updated signals and slots for doors

--- elevator/doors.cpp
+++ elevator/doors.cpp
@@ -1,33 +1,39 @@
 #include "doors.h"
+#include <QTimer>
 
-//Doors::Doors()
-//{
-//    QObject::connect();
-//}
+Doors::Doors()
+{
+    QObject::connect(this, SIGNAL(open()), this, SLOT(openDoorsSlot()));
+    QObject::connect(this, SIGNAL(opened()), this, SLOT(doorsOpenedSlot()));
+    QObject::connect(this, SIGNAL(close()), this, SLOT(closeDoorsSlot()));
+    QObject::connect(this, SIGNAL(closed()), this, SLOT(doorsClosedSlot()));
+}
 
 
 //// SLOTS
-//void Doors::liftArrivedSlot()
-//{
-//    emit open();
-//}
+void Doors::liftArrivedSlot()
+{
+    emit open();
+}
 
-//void Doors::openDoorsSlot()
-//{
+void Doors::openDoorsSlot()
+{
 //    emit opened(); // timer
-//}
+    QTimer::singleShot(5000, this, SLOT(doorsOpenedSlot()));
+}
 
-//void Doors::doorsOpenedSlot()
-//{
-//    emit close();
-//}
+void Doors::doorsOpenedSlot()
+{
+    emit close();
+}
 
-//void Doors::closeDoorsSlot()
-//{
+void Doors::closeDoorsSlot()
+{
 //    emit closed(); // timer
-//}
+    QTimer::singleShot(5000, this, SLOT(doorsClosedSlot()));
+}
 
-//void Doors::doorsClosedSlot()
-//{
-//    emit termitaned();
-//}
+void Doors::doorsClosedSlot()
+{
+    emit terminated();
+}
