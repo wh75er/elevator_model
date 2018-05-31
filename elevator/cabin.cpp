c9cf46104dd07fb5fa347a851549a8822518507d
Nick <stroooooom@gmail.com>
2018-05-31 18:13:44 +0300

    updated

--- elevator/cabin.cpp
+++ elevator/cabin.cpp
@@ -3,11 +3,11 @@
 #include <iostream>
 #include <cstring>
 
-#define MOVING_TIME 1000
+#define MOVING_TIME 5000
 
 Cabin::Cabin()
 {
-    this->timer.setSingleShot(false);
+    this->timer.setSingleShot(true);
 
     QObject::connect(this, SIGNAL(openDoors()), &this->doors, SLOT(openDoorsSlot()));
     QObject::connect(&this->doors, SIGNAL(doorsOpened()), this, SLOT(stayOpenedSlot()));
@@ -17,6 +17,8 @@ Cabin::Cabin()
 
 void Cabin::movingUpSlot() // <- movingUp
 {
+    std::cout << ">> movingUpSlot()\n";
+
     this->current_state = MOVING_UP;
     this->timer.start(MOVING_TIME);
 }
@@ -24,6 +26,8 @@ void Cabin::movingUpSlot() // <- movingUp
 
 void Cabin::movingDownSlot() // <- movingDown
 {
+    std::cout << ">> movingDownSlot()\n";
+
     this->current_state = MOVING_DOWN;
     this->timer.start(MOVING_TIME);
 }
--- elevator/lift.cpp
+++ elevator/lift.cpp
@@ -4,7 +4,6 @@
 Lift::Lift()
 {
     QTimer::connect(&this->cabin.timer, SIGNAL(timeout()), &this->controller, SLOT(movedToFloorSlot()));
-    QTimer::connect(&this->cabin.timer, SIGNAL(timeout()), &this->controller, SLOT(movedToFloorSlot()));
 
     QTimer::connect(&this->cabin.doors, SIGNAL(doorsClosed()), &this->controller, SLOT(stayClosedSlot()));
 
